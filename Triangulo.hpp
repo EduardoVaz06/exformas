581d10b09ac851f762b3c69dd0fbcd834fd46c7d
EduardoVaz06 <83786194+EduardoVaz06@users.noreply.github.com>
2021-05-28 10:04:45 -0300

    Update Triangulo.hpp

--- Triangulo.hpp
+++ Triangulo.hpp
@@ -1,1 +1,30 @@
+#include "Formas.hpp"
+
+
+#include <iostream>
+using namespace std;
+
+#ifndef Triangulo_hpp
+#define Triangulo_hpp
+
+
+class Triangulo : public Formas
+{
+public:
+    Triangulo(){};
+    Triangulo(string n, float b, float h);
+
+    void calculaArea();
+
+    void setBase(float b);
+    void setAltura(float a);
+
+
+private:
+    float base;
+    float altura;
+};
+
+#endif
+
 
