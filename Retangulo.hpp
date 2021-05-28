081d41179c9ec333f9af836b1dad04f84aa05909
EduardoVaz06 <83786194+EduardoVaz06@users.noreply.github.com>
2021-05-28 10:04:04 -0300

    Update Retangulo.hpp

--- Retangulo.hpp
+++ Retangulo.hpp
@@ -1,1 +1,31 @@
+#include "Formas.hpp"
+
+
+#include <iostream>
+using namespace std;
+
+#ifndef Retangulo_hpp
+#define Retangulo_hpp
+
+
+class Retangulo : public Formas
+{
+public:
+    Retangulo(){};
+    Retangulo(string n, float b, float h);
+
+    void calculaArea();
+
+    void setBase(float b);
+    void setAltura(float a);
+
+
+private:
+    float base;
+    float altura;
+};
+
+#endif
+
+
 
