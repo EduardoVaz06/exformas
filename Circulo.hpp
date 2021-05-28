e8010e597eed8e7a68f09b677f207c77e1633b79
EduardoVaz06 <83786194+EduardoVaz06@users.noreply.github.com>
2021-05-28 10:00:51 -0300

    Update Circulo.hpp

--- Circulo.hpp
+++ Circulo.hpp
@@ -1,15 +1,22 @@
-#include "Forma.hpp"
+#include "Formas.hpp"
+
+
+#include <iostream>
+using namespace std;
 
 #ifndef Circulo_hpp
 #define Circulo_hpp
 
 
-class Circulo : public Forma
+class Circulo : public Formas
 {
 public:
+    Circulo(){};
+    Circulo(string n, float r);
+
     void calculaArea();
 
-    void setRaio(float raio);
+    void setRaio(float r);
 
 
 private:
@@ -17,4 +24,3 @@ private:
 };
 
 #endif
-
