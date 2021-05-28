8d981269840a5ad988ae7d7e969dd68e2ee3d62e
EduardoVaz06 <83786194+EduardoVaz06@users.noreply.github.com>
2021-05-28 10:01:44 -0300

    Update Retangulo.cpp

--- Retangulo.cpp
+++ Retangulo.cpp
@@ -1,1 +1,27 @@
+#include "Retangulo.hpp"
+
+Retangulo::Retangulo(string n, float b, float a)
+{
+    nome = n;
+    base = b;
+    altura = a;
+}
+
+void Retangulo::setBase(float b)
+{
+    base = b;
+}
+
+void Retangulo::setAltura(float a)
+{
+    altura = a;
+}
+
+void Retangulo::calculaArea()
+{
+    area = (altura*base);
+    cout << "Area do Retangulo " << nome << " = " << area << endl;
+}
+
+
 
