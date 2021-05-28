da4b26ee4eb5ef17b42233bfcd900e2786cabf07
EduardoVaz06 <83786194+EduardoVaz06@users.noreply.github.com>
2021-05-28 10:04:25 -0300

    Update Triangulo.cpp

--- Triangulo.cpp
+++ Triangulo.cpp
@@ -1,1 +1,26 @@
+#include "Triangulo.hpp"
+
+Triangulo::Triangulo(string n, float b, float a)
+{
+    nome = n;
+    base = b;
+    altura = a;
+}
+
+void Triangulo::setBase(float b)
+{
+    base = b;
+}
+
+void Triangulo::setAltura(float a)
+{
+    altura = a;
+}
+
+void Triangulo::calculaArea()
+{
+    area = ((altura*base)/2);
+    cout << "Area do Triangulo " << nome << " = " << area << endl;
+}
+
 
